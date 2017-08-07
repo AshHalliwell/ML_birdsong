3a2d8b5f154f231cdb19c4f29e8b2ca8324be450
Ash Halliwell <halliwellAsh@gmail.com>
2017-08-07 17:54:51 +1200

    Changed file naming conventions

--- ash_expt.py
+++ ash_expt.py
@@ -25,21 +25,25 @@ print('Length of file is: '+str(float(len(audio[0])/(no_channels*frequency)))+' 
 #for i in audio:
 #	print(i)
 
+if not os.path.exists(source):
+    os.makedirs(source)
+
 duration = 1 #Half a second
 offset = 0;
 #attempting to slice
 while(offset + duration < float(len(audio[0])/(no_channels*frequency))):
+
     suffix = str(offset) + "-" + str(offset + duration)
-    wav_file = open("audio/stitchout"+ suffix + ".wav", 'w')
+    wav_file = open(source + "/audio/audio_"+ suffix + ".wav", 'w')
     wavy.slice_wave(source, wav_file, offset, duration) #input file, output file, start in seconds, duration in seconds
     wav_file.close()
     #print("Segment: " + suffix)
-    fragment = wavy.get_audio("audio/stitchout"+ suffix + ".wav")
+    fragment = wavy.get_audio(source + "/audio/audio_"+ suffix + ".wav")
 
     #spectrogram of sliced file
     plt.figure(1)
     nfft=1024
     fs=256#Sampling frequency
     Pxx, freqs, bins, im = plt.specgram(fragment[0], nfft, fs)
-    plt.savefig('imgs/stitch_specgram' + suffix + '.jpg', dpi=100)
+    plt.savefig(source + 'imgs/specgram_' + suffix + '.jpg', dpi=100)
     offset += duration
