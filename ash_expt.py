cfcc28a00ba16dd840b8603b7cb79f0c508bca16
Ash Halliwell <halliwellAsh@gmail.com>
2017-08-28 12:02:56 +1200

    Testing cmap

--- ash_expt.py
+++ ash_expt.py
@@ -2,7 +2,7 @@ import os, wave
 import numpy as np
 import datetime
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 #from pylab import figure, specgram, savefig, imshow
 import wavy
@@ -12,7 +12,7 @@ import sys
 source=sys.argv[1]
 audio=wavy.get_audio(source)
 
-print(audio)
+print audio
 
 print(len(audio[0]))
 
@@ -27,33 +27,27 @@ print('Length of file is: '+str(float(len(audio[0])/(no_channels*frequency)))+' 
 
 if not os.path.exists(source+"_results"):
     os.makedirs(source+"_results")
-if not os.path.exists(source+"/audio"):
-    os.makedirs(source+"/audio")
-if not os.path.exists(source+"/imgs"):
-    os.makedirs(source+"/imgs")
+if not os.path.exists(source+"_results/audio"):
+    os.makedirs(source+"_results/audio")
+if not os.path.exists(source+"_results/imgs"):
+    os.makedirs(source+"_results/imgs")
 
 duration = 1 #Half a second
 offset = 0;
 #attempting to slice
-while(offset + duration < float(len(audio[0])/(no_channels*frequency))):
+while(offset + duration < 3):#float(len(audio[0])/(no_channels*frequency))):
 
     suffix = str(offset) + "-" + str(offset + duration)
-    wav_file = open(source + "/audio/audio_"+ suffix + ".wav", 'w')
+    wav_file = open(source + "_results/audio/audio_"+ suffix + ".wav", 'w')
     wavy.slice_wave(source, wav_file, offset, duration) #input file, output file, start in seconds, duration in seconds
     wav_file.close()
     #print("Segment: " + suffix)
     fragment = wavy.get_audio(source + "_results/audio/audio_"+ suffix + ".wav")
-    fragment = wavy.get_audio(source + "/audio/audio_"+ suffix + ".wav")
+
     #spectrogram of sliced file
-    fig = plt.figure(1)
+    plt.figure(1)
     nfft=1024
     fs=256#Sampling frequency
     Pxx, freqs, bins, im = plt.specgram(fragment[0], nfft, fs, cmap='binary')
-    #for i in range(len(Pxx[0][:]))
-    #    someArray[i] = sum(Pxx[0][i])
-    plt.savefig(source + '_results/imgs/' + source + '_specgram_' + suffix + '.jpg', dpi=100)
-    #plt.plot(i, someArray[i])
-    #plt.savefig(source + '_results/imgs/' + source + '_specgram_' + suffix + '.jpg', dpi=100)
-    Pxx, freqs, bins, im = plt.specgram(fragment[0], nfft, fs)
-    plt.savefig(source + 'imgs/specgram_' + suffix + '.jpg', dpi=100)
+    plt.savefig(source + '_results/imgs/specgram_' + suffix + '.jpg', dpi=100)
     offset += duration
